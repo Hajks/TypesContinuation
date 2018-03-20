f2969d18d4ce58edefcefb6d8588ea578490eecd
Hajk <tunczorx@gmail.com>
2018-03-20 12:42:43 +0100

    Just additional infromation for types

--- TypesContinuation/Form1.cs
+++ TypesContinuation/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace TypesContinuation
 {
     public partial class Form1 : Form
@@ -15,6 +16,32 @@ namespace TypesContinuation
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            decimal myDecimalValue = 10;
+            // int myIntValue = myDecimalValue; it won't works because we try to convert decimal to int, but we can use projection(?)/rzutowanie
+            int myIntValue = (int)myDecimalValue;
+            int x = 365;
+            byte myByte = (byte) x; // As we can see there is no error, but if we check what value is behind x we will see it's 109 because max of byte possibilitie is 256 and if we add 109 it will equal 365!
+            MessageBox.Show("Liczba x to: " + myByte);
+
+            int myInt = 36;
+            float myFloat = 16.4F;
+            myFloat = myInt + myFloat; // if we use operators like "+","-","/","*" it will do projection automatically to bigger type. Float can be included in int type, but int type can't be included in float type. That's why compilator convert to int type.
+
+            long l = 139401930;
+            short s = 516;
+            double d = l - s; // we used two operators "-" and "=", minus was used to substract long from short and operator "is equal to" to convert to double type
+            d = d / 123.456;
+            MessageBox.Show("Liczba d to: " + d);
+
+            long b = 139401930;
+            MessageBox.Show("Liczba b to: " + b); // operator + which allows us convert number b to string
+            // wont work MessageBox.Show(b);
+            MessageBox.Show(b.ToString()); // simple method To.String changed value b to string but without using any of operators
+
         }
     }
 }
